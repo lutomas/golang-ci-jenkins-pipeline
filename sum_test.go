f18bf294f60ee0b094e40a62ac8e206eefd8dea9
Tomas Lukosius <tomas@unfuzz.com>
2019-03-20 16:16:43 +0200

    unFixed test.

--- sum_test.go
+++ sum_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestSum(t *testing.T) {
 	actual := Sum(1, 2)
-	expected := 2
+	expected := 3
 	if actual != expected {
 		t.Errorf("Sum(1, 2): actual %v, expected %v", actual, expected)
 	}
